1b890ecb9b1010891905231766fe3509f99582da
Basel Sayed <baselmohamed802@gmail.com>
2025-12-04 14:37:30 +0200

    Create handTrackingModule.py

---
+++ MediaPipe Handtracking/Modules/handTrackingModule.py
@@ -0,0 +1,9 @@
+"""
+handTrackingModule.py
+Author: Basel Mohamed Mostafa Sayed
+Date: 12/4/2025
+
+Description:
+    This file contains helping functions (utility functions) for the hand tracking module
+    to be used for the google mediapipe library.
+"""
